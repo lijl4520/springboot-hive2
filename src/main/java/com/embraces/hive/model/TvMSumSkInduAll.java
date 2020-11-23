12686bb80a8ff2a7dca0d0f7c866bed312a00f20
root <admin@example.com>
2020-11-23 15:43:44 +0800

    [fix][]添加行业接口

---
+++ src/main/java/com/embraces/hive/model/TvMSumSkInduAll.java
@@ -0,0 +1,22 @@
+package com.embraces.hive.model;
+
+/**
+ * @Author Lijl
+ * @ClassName TvMSumSkIndu
+ * @Description TODO
+ * @Date 2020/11/23 14:38
+ * @Version 1.0
+ */
+public class TvMSumSkInduAll {
+
+    //服务号码
+    private String SERV_NUMBER;
+
+    public String getSERV_NUMBER() {
+        return SERV_NUMBER;
+    }
+
+    public void setSERV_NUMBER(String SERV_NUMBER) {
+        this.SERV_NUMBER = SERV_NUMBER;
+    }
+}
